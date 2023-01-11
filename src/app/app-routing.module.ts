f836f686181dcd519b3735650af0e5c44416fea7
ElvisStwar <elvis.stwar@hotmail.com>
2023-01-11 10:00:54 -0500

    Creacion de rutas e implementacion de bootstrap CDN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:"home", component: HomeComponent},
+  {path:"", component: HomeComponent},
+  {path:"login", component :LoginComponent},
+  {path:"404",component: NotFoundComponent},
+  {path:"**",pathMatch:"full",redirectTo:"404"}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,16 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule
-  ]
+  ],
+  exports: [
+    HomeComponent,
+    LoginComponent,
+    NotFoundComponent
+  ],
 })
 export class PagesModule { }
